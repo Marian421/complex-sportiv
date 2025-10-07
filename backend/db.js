c1cfe4ecbbc8542f631d9bd73f26587e8f88f2a7
Herciu Marian <herciumarian8@gmail.com>
2025-10-07 21:51:44 +0300

    Fix db.js to connect correctly to Supabase

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,10 @@
+const { connectionString } = require('pg/lib/defaults');
+
 require('dotenv').config();
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL
 })
 
 if (process.env.NODE_ENV !== 'test'){
